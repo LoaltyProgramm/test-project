fbb9fd244d1afd94d131d2068882527766824b2f
Bubnov Ivan <bubnov.ivanbubnoff@yandex.ru>
2025-03-28 00:11:24 +0300

    hot fix main.go

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,12 @@ import "fmt"
 
 func main() {
   a := 1 + 1
-  sum := a *3
-  fmt.Println(sum)
+  sum := a / 3
+  if sum > 2 {
+	fmt.Println(sum)
+  } else {
+	fmt.Println("No fix")
+  }
+  
 }
 
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/LoaltyProgramm/test-project
+
+go 1.23.6
